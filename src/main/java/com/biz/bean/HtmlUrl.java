15c374af789c4ab531ad205c5548fbb9fb28accf
Raj <Raj@LAPTOP-4PQQGIGM>
2019-10-16 00:15:40 +0530

    Html Url Entity and Bo added

---
+++ src/main/java/com/biz/bean/HtmlUrl.java
@@ -0,0 +1,59 @@
+package com.biz.bean;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "html_url")
+public class HtmlUrl implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid")
+	private String id;
+
+	@NotNull
+	@Size(max = 100)
+	@Column(name = "url_text")
+	private String text;
+
+	@Size(max = 100)
+	@Column(name = "url_href")
+	private String href;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getHref() {
+		return href;
+	}
+
+	public void setHref(String href) {
+		this.href = href;
+	}
+
+}
